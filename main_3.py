fb97d536ffb1177a15a88469d10b1a1fd024b72b
andrei <andreimendes8@gmail.com>
2024-09-04 10:05:14 -0300

    Insertion Sort

--- main_3.py
+++ main_3.py
@@ -1,10 +1,18 @@
+ #  Elaborar um função em Python para ordenar uma lista utilizando a ordenação por 
+# inserção (Insertion Sort). 
 
+def insertion_sort(list):
+    for j in range(1, len(list)):
+        key = list[j]
+        i = j - 1
+        
+        while i >= 0 and list[i] > key:
+            list[i + 1] = list[i]
+            i -= 1
+        
+        list[i + 1] = key
 
-  InsertionSort(arr[])
-      for j = 1 to arr.length
-         key = arr[j]
-         i = j - 1
-         while i >= 0 and arr[i] > key
-            arr[i+1] = arr[i]
-            i = i - 1
-         arr[i+1] = key
+list = [9, 5, 1, 4, 3]
+insertion_sort(list)
+print("Lista ordenada:", list)
+
