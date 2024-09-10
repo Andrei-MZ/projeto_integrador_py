94f94aeb76f682f2f170560b11cb3f8982ba9211
andrei <andreimendes8@gmail.com>
2024-09-10 07:57:22 -0300

    tabela

---
+++ main_4.py
@@ -0,0 +1,10 @@
+def tabela_verdade_xor():
+    print("A | B | A XOR B")
+    print("--|---|--------")
+    for a in [0, 1]:
+        for b in [0, 1]:
+            resultado = a ^ b 
+            print(f"{a} | {b} |    {resultado}")
+
+
+tabela_verdade_xor()
