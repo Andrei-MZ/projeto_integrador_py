750e700d3e8efa06db995a8b5998acd3e89bfa4b
andrei <andreimendes8@gmail.com>
2024-09-10 08:04:52 -0300

    map

--- main_5.py
+++ main_5.py
@@ -1,5 +1,7 @@
-def separate_numbers(list):
+def separate_numbers(lista):
     positivos = [num for num in lista if num >= 0]
     negativos = [num for num in lista if num < 0]
 
     return positivos, negativos
+
+numeros = list(map(int, input("Digite uma lista de números inteiros separados por espaço: ").split()))
